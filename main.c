0b9ed1984775480539544e7de9b980b62aadf7c0
Вика <PRIb-212_254344@volsu.ru>
2023-03-01 18:00:44 +0300

    Добавлен вывод суммы чисел на экран

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 
 int main()
 {
-    int a, b;
+    int A, B;
     printf("Enter A(integer):\n");
-    scanf("%d", &a);
+    scanf("%d", &A);
     printf("Enter B(integer):\n");
-    scanf("%d", &b);
+    scanf("%d", &B);
+    printf("A + B = %d\n", A+B);
     return 0;
 }
