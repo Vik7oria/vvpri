0e479e7a25bdbc14709367bf2b0496c68f700ffe
Вика <PRIb-212_254344@volsu.ru>
2023-03-01 17:42:49 +0300

    Добавлены файлы main.c и std.h. Написана программа для ввода целых чисел с клавиатуры.

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "std.h"
+
+int main()
+{
+    int a, b;
+    printf("Enter A(integer):\n");
+    scanf("%d", &a);
+    printf("Enter B(integer):\n");
+    scanf("%d", &b);
+    return 0;
+}
